7006a3087a580d187972337d4d841bb5aea08453
icomfort <a1997228@yahoo.com.tw>
2020-03-22 15:07:19 +0800

    第三次

--- commerce/src/main.js
+++ commerce/src/main.js
@@ -5,12 +5,38 @@ import jQuery from 'jquery'
 import {
   fb
 } from './firebase'
+import VueFirestore from 'vue-firestore'
 
+require('firebase/firestore')
+
+Vue.use(VueFirestore, {
+  key: 'id', // the name of the property. Default is '.key'.
+  enumerable: true //  whether it is enumerable or not. Default is true.
+})
+
+
+Vue.use(VueFirestore)
 
 window.$ = window.jQuery = jQuery
 import 'popper.js'
 import 'bootstrap'
 import './assets/app.scss'
+
+
+import Swal from 'sweetalert2';
+
+window.Swal = Swal;
+
+
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000
+});
+
+window.Toast = Toast;
+
 
 Vue.component('Navbar', require('./components/Navbar.vue').default)
 
--- commerce/src/views/Products.vue
+++ commerce/src/views/Products.vue
@@ -4,40 +4,18 @@
       <div class="intro h-100">
         <div class="row h-100 justify-content-center align-items-center">
           <div class="col-md-6">
-            <h3>Products Page</h3>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, ducimus.</p>
+            <h3>產品頁面</h3>
+            <p>在這裡，您可以管理您的商品。</p>
           </div>
           <div class="col-md-6">
             <img src="/img/svg/overview.svg" alt class="img-fluid" />
           </div>
         </div>
       </div>
-      <hr />
-      <h3>商品管理</h3>
-      <p>新增、刪除、修改您的產品。</p>
-      <div class="produuct-test">
-        <div class="form-group">
-          <input
-            type="text"
-            placeholder="產品名稱"
-            v-model="product.name"
-            class="form-control col-md-3"
-          />
-        </div>
-        <div class="form-group">
-          <input
-            type="text"
-            placeholder="產品價格"
-            v-model="product.price"
-            class="form-control col-md-3 justify-content-center"
-          />
-        </div>
-        <div class="form-group">
-          <button @click="saveData" class="btn btn-primary">保存</button>
-        </div>
-        <hr />
 
-        <h3>產品列表</h3>
+      <div class="product-test">
+        <h3 class="d-inline-block">產品列表</h3>
+        <button @click="addNew" class="btn btn-primary float-right">新增產品</button>
 
         <div class="table-responsive">
           <table class="table">
@@ -51,11 +29,11 @@
 
             <tbody>
               <tr v-for="product in products">
-                <td>{{product.data().name}}</td>
-                <td>{{product.data().price}}</td>
+                <td>{{ product.name }}</td>
+                <td>{{ product.price }}</td>
                 <td>
-                  <button class="btn btn-primary">Edit</button>
-                  <button @click="deleteProduct(product.id)" class="btn btn-danger">Delete</button>
+                  <button class="btn btn-primary" @click="editProduct(product)">修改</button>
+                  <button class="btn btn-danger" @click="deleteProduct(product)">刪除</button>
                 </td>
               </tr>
             </tbody>
@@ -63,69 +41,191 @@
         </div>
       </div>
     </div>
+
+    <!-- Modal -->
+    <div
+      class="modal fade"
+      id="product"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="editLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="editLabel">編輯產品</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="row">
+              <!-- main product -->
+              <div class="col-md-8">
+                <div class="form-group">
+                  <input type="text" placeholder="產品名稱" v-model="product.name" class="form-control" />
+                </div>
+
+                <div class="form-group">
+                  <vue-editor v-model="product.description"></vue-editor>
+                </div>
+              </div>
+              <!-- product sidebar -->
+              <div class="col-md-4">
+                <h4 class="display-6">產品資訊</h4>
+                <hr />
+
+                <div class="form-group">
+                  <input
+                    type="text"
+                    placeholder="產品價格"
+                    v-model="product.price"
+                    class="form-control"
+                  />
+                </div>
+
+                <div class="form-group">
+                  <input
+                    type="text"
+                    @keyup.188="addTag"
+                    placeholder="產品標籤"
+                    v-model="tag"
+                    class="form-control"
+                  />
+
+                  <div class="d-flex">
+                    <p v-for="tag in product.tags">
+                      <span class="p-1">{{ tag }}</span>
+                    </p>
+                  </div>
+                </div>
+
+                <div class="form-group">
+                  <label for="product_image">產品圖</label>
+                  <input type="file" @change="uploadImage" class="form-control" />
+                </div>
+
+                <div class="form-group d-flex">
+                  <div class="p-1" v-for="(image, index) in product.images">
+                    <div class="img-wrapp">
+                      <img :src="image" alt width="80px" />
+                      <span class="delete-img" @click="deleteImage(image, index)">X</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button
+              @click="addProduct()"
+              type="button"
+              class="btn btn-primary"
+              v-if="modal=='new'"
+            >新增</button>
+            <button
+              @click="updateProduct()"
+              type="button"
+              class="btn btn-primary"
+              v-if="modal=='edit'"
+            >保存</button>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">關閉</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { VueEditor } from 'vue2-editor'
 import { fb, db } from '../firebase'
 
 export default {
-  name: "Products",
+  name: 'Products',
+  components: {
+    VueEditor
+  },
   props: {
     msg: String
   },
 
   data() {
     return {
-      products: [],
       product: {
         name: null,
-        price: null
+        description: null,
+        price: null,
+        tags: [],
+        images: []
+      },
+      activeItem: null,
+      modal: null,
+      tag: null
+    }
+  },
 
-      }
+  firestore() {
+    return {
+      products: db.collection('products')
     }
   },
   methods: {
-    deleteProduct(doc) {
-      if (confirm('確定要刪除此項目嗎?')) {
-        db.collection("products").doc(doc).delete().then(function () {
-          console.log("Document successfully deleted!");
-        }).catch(function (error) {
-          console.error("Error removing document: ", error);
-        });
-      } else {
+    uploadImage() { },
 
-      }
+    addNew() {
+      this.product = {}
+      this.modal = 'new'
+      $('#product').modal('show')
     },
-    readData() {
-      db.collection("products").get().then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          this.products.push(doc);
-        })
+    updateProduct() {
+      this.$firestore.products.doc(this.product.id).update(this.product);
+      Toast.fire({
+        type: 'success',
+        title: '更新成功'
+      })
+
+      $('#product').modal('hide');
+    },
+    editProduct(product) {
+      this.modal = 'edit'
+      this.product = product
+      $('#product').modal('show')
+    },
+    deleteProduct(doc) {
+      Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then(result => {
+        if (result.value) {
+          this.$firestore.products.doc(doc.id).delete()
+
+          Toast.fire({
+            type: 'success',
+            title: '刪除成功'
+          })
+        }
       })
     },
-    saveData() {
-      db.collection("products").add(this.product)
-        .then((docRef) => {
-          alert("成功");
-          console.log("寫入ID:", docRef.id);
-          this.readData();
-          this.reset();
-        })
-        .catch(function (erroe) {
-          console.error("保存錯誤:", error);
-        })
-    },
-    reset() {
-      Object.assign(this.$data, this.$options.data.apply(this))
+    readData() { },
+    addProduct: function () {
+      this.$firestore.products.add(this.product)
+      Toast.fire({
+        type: 'success',
+        title: '新增成功'
+      })
+      $('#product').modal('hide')
     }
   },
-  created() {
-    this.readData();
-  }
-};
+  created() { }
+}
 </script>
-// 
+//
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
